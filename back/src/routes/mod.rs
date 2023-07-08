6887575f15605a13fcc23e895fc970a0699f8c6f
krirogn <kf@krirogn.dev>
2023-07-09 01:45:38 +0200

    Index route

--- back/src/routes/mod.rs
+++ back/src/routes/mod.rs
@@ -5,9 +5,10 @@ mod get_business;
 mod remove_business;
 mod update_business;
 mod v;
+mod index;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(v::get_version).service(
+    cfg.service(v::get_version).service(index::index).service(
         web::scope("/business")
             .service(add_business::add_business)
             .service(get_business::get_business)
